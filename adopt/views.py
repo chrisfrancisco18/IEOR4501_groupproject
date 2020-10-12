b06179dfe3f6a13411991e79e09ca81797d42182
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-12 18:59:34 -0400

    Add sightings
    Need to create sightings.html in adopt/templates/adopt which will show Fields:
    Unique Squirrel ID
    Date
    Link to unique squirrel sighting

--- adopt/views.py
+++ adopt/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+
+from .models import Squirrel
 
 # Create your views here.
 def index(request):
     return render(request, 'adopt/index.html',{})
 
+# First view 
+# see Squirrel Tracker Doc
+"""
+def map(request):
+    context = {}
+    return render(request, 'adopt/map.html', context)
+"""
+
+# Second view
+# see Squirrel Tracker Doc
+def sightings(request, squirrel.id):
+    squirrel = get_object_or_404(Squirrel, pk=squirrel.id)
+    
+    context = {
+       'squirrel':squirrel, 
+    }
+    
+    return render(request, 'adopt/sightings.html', context)
+    
