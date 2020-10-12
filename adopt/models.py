0163ae0907ef08f6038d71ea29673d7eabd0f6eb
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-12 19:43:06 -0400

    Update models.py
    Update each field

--- adopt/models.py
+++ adopt/models.py
@@ -1,23 +1,77 @@
-rom django.db import models
+from django.db import models
+from django.utils.translation import gettext as _
+
+# Create your models here
 
 class Squirrel(models.Model):
-    X=models.FloatField()
-    Y=models.FloatField()
+    X=models.FloatField(
+        help_text=_("Latitude"),
+    )
+    Y=models.FloatField(
+        help_text=_("Longitude"),
+    )
     Unique_Squirrel_ID=models.CharField(
-            max_length=40)
+        max_length=14,
+        help_text=_("The Unique ID of Squirrel Sighting"),
+    )
     Hectare=models.CharField(
-            max_length=10)
+        max_length=4,
+    )
+    
+    # Only two choice for Shift
+    AM = 'AM'
+    PM = 'PM'
+    
+    SHIFT_CHOICES = [
+        (AM, _('AM')),
+        (PM, _('PM')),
+    ]
+    
     Shift=models.CharField(
-            max_length=10)
+        max_length=3,
+        choices=SHIFT_CHOICES,
+        default=AM,
+    )
+    
     Date=models.CharField(
-            max_length=40)
-    Hectare_squirrel_nunber=models.IntegerField()
+        max_length=9
+        help_text=_("Date"),
+    )
+    
+    Hectare_squirrel_nunber=models.IntegerField(
+        help_text=_("Hectare Number"),
+    )
+    
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    OTHER = ''
+    
+    AGE_CHOICES = [
+        (ADULT, _('Adult')),
+        (JUVENILE, _('Juvenile')),
+        (OTHER, _('')),
+    ]
+    
     Age=models.CharField(
-            max_length=20)
+        blank=True,
+        max_length=20,
+        help_text=_("Sex"),
+        choices=AGE_CHOICES,
+        default=OTHER,
+    )
+    
     Primary_Fur_Color=models.CharField(
-            max_length=20)
+        blank=True,
+        max_length=20
+        help_text=_("Primary Fur Color"),
+    )
+    
     Highlight_Fur_Color=models.CharField(
-            max_length=20)
+        blank=True,
+        max_length=20,
+        help_text=_("Highlight Fur Color"),
+    )
+    
     Combi_Fur_Color=models.CharField(
             max_length=40)
     Color_Notes=models.CharField(
@@ -47,6 +101,6 @@ class Squirrel(models.Model):
             max_length=100)
     Lat_Long=models.CharField(
             max_length=100)
-
-# Create your models here
-
+    
+    def __str__(self):
+        return self.Unique_Squirrel_ID
