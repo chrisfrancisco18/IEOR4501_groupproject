c16eb629155724fef4b4f16e867d6fa5d596fa9b
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-12 19:11:38 -0400

    Update urls.py
    Update for sightings

--- adopt/urls.py
+++ adopt/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
+# Should we change the name?
+app_name = 'adopt'
+
 urlpatterns = [
-        path('', views.index),
+        #path('', views.index),
+        path('', views.sightings),
         ]
