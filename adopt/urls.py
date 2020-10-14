db2cf1b4d7375842f220a51f9c2f2acb8c9290a0
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-14 02:06:28 -0400

    Update urls.py

--- adopt/urls.py
+++ adopt/urls.py
@@ -5,7 +5,12 @@ from . import views
 app_name = 'adopt'
 
 urlpatterns = [
-        #path('', views.index),
-        path('sightings/', views.sightings),
-        path('sightings/<int:squirrel_id>/', views.detail, name='detail'),
-        ]
+        # path('', views.index),
+        path('sightings/', include(extrapatterns)),
+]
+
+extrapatterns = [
+        path('', views.sightings),
+        path('<int:squirrel_id>/', views.detail, name='detail'),
+        # path('<slug:squirrel_Unique_Squirrel_ID>/', views.detail, name='detail'),
+]
