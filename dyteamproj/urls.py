0652e78a2a2e0bc9abdb12c3da722aafd8401bfa
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-14 01:08:31 -0400

    Update urls.py

--- dyteamproj/urls.py
+++ dyteamproj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adopt/', include('adopt.urls')),
+    path('adopt/sightings/', include('adopt.urls')),
 ]
