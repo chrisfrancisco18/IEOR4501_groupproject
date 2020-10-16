435c791908cb5fc060f7bed3bc2d27f6e859d34e
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-16 14:46:19 -0400

    Create forms.py

---
+++ adopt/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+
+from .models import Squirrel
+
+class NameForm(ModelForm):
+  class Meta:
+    model = Squirrel
+    fields = [
+      # update this later
+      'Lattitude',
+      'Longitude',
+      ]
