7c2e18a35d38e269c94f625f0b0fe9ba5a74722a
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-16 18:02:36 -0400

    Update forms.py

--- adopt/forms.py
+++ adopt/forms.py
@@ -5,33 +5,34 @@ from django.utils.translation import gettext as _
 from .models import Squirrel
 
 class SquirrelForm(ModelForm):
-    #your_name = forms.CharField(label='Your name', max_length=100)
+    class Meta:
+        #your_name = forms.CharField(label='Your name', max_length=100)
 
-    sq_lat = forms.FloatField(label='Latitude')
-    sq_longi = forms.FloatField(label='Longitude')
-    sq_id = forms.SlugField(label='Unique ID', max_length =14)
-    AM = "AM"
-    PM = "PM"
-    SHIFT_CHOICES = [
-        (AM, _('AM')),
-        (PM, _('PM')),
-        ]
+        sq_lat = forms.FloatField(label='Latitude')
+        sq_longi = forms.FloatField(label='Longitude')
+        sq_id = forms.SlugField(label='Unique ID', max_length =14)
+        AM = "AM"
+        PM = "PM"
+        SHIFT_CHOICES = [
+            (AM, _('AM')),
+            (PM, _('PM')),
+            ]
         
-    sq_shift =forms.CharField(max_length=3, 
-          #choices= SHIFT_CHOICES, default=AM
-                             )
-    sq_date = forms.DateField(
-        #auto_now=False, auto_now_add=False
-    )
-    ADULT = 'Adult'
-    JUVENILE = 'Juvenile'
-    OTHER = ''
-    AGE_CHOICES = [
-        (ADULT, _('Adult')),
-        (JUVENILE, _('Juvenile')),
-        (OTHER, _('')),
-    ]
-    sq_age = forms.CharField(
+        sq_shift =forms.CharField(max_length=3, 
+            #choices= SHIFT_CHOICES, default=AM
+            )
+        sq_date = forms.DateField(
+            #auto_now=False, auto_now_add=False
+        )
+        ADULT = 'Adult'
+        JUVENILE = 'Juvenile'
+        OTHER = ''
+        AGE_CHOICES = [
+            (ADULT, _('Adult')),
+            (JUVENILE, _('Juvenile')),
+            (OTHER, _('')),
+        ]
+        sq_age = forms.CharField(
             #blank=True,
             max_length=20,
             #choices=AGE_CHOICES,
