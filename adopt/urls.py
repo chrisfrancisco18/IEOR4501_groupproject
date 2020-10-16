d952f741d14aa66a59862825953465a9291587ea
Peeratach Ritthikarn <pr2638@columbia.edu>
2020-10-16 21:20:03 +0000

    add templates

--- adopt/urls.py
+++ adopt/urls.py
@@ -8,6 +8,7 @@ extra_patterns = [
         path('', views.sightings, name='sightings'),
         # path('<int:squirrel_id>/', views.detail, name='detail'),
         path('<slug:Unique_Squirrel_ID>/', views.squirrel_detail, name='detail'),
+        path('add/', views.add_sighting, name='add'),
 ]
 
 urlpatterns = [
