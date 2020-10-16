3dcf0e23065784f05423833b22aefd3e10e37441
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-16 18:07:09 -0400

    Update forms.py

--- adopt/forms.py
+++ adopt/forms.py
@@ -4,37 +4,36 @@ from django.forms import ModelForm
 from django.utils.translation import gettext as _
 from .models import Squirrel
 
-class SquirrelForm(ModelForm):
-    class Meta:
+class SquirrelForm(Forms.Form):
+    #class Meta:
         #your_name = forms.CharField(label='Your name', max_length=100)
-
-        sq_lat = forms.FloatField(label='Latitude')
-        sq_longi = forms.FloatField(label='Longitude')
-        sq_id = forms.SlugField(label='Unique ID', max_length =14)
-        AM = "AM"
-        PM = "PM"
-        SHIFT_CHOICES = [
-            (AM, _('AM')),
-            (PM, _('PM')),
-            ]
+    sq_lat = forms.FloatField(label='Latitude')
+    sq_longi = forms.FloatField(label='Longitude')
+    sq_id = forms.SlugField(label='Unique ID', max_length =14)
+    AM = "AM"
+    PM = "PM"
+    SHIFT_CHOICES = [
+        (AM, _('AM')),
+        (PM, _('PM')),
+    ]
         
-        sq_shift =forms.CharField(max_length=3, 
+    sq_shift =forms.CharField(max_length=3, 
             #choices= SHIFT_CHOICES, default=AM
-            )
-        sq_date = forms.DateField(
+                             )        
+    sq_date = forms.DateField(
             #auto_now=False, auto_now_add=False
-        )
-        ADULT = 'Adult'
-        JUVENILE = 'Juvenile'
-        OTHER = ''
-        AGE_CHOICES = [
+    )
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    OTHER = ''
+    AGE_CHOICES = [
             (ADULT, _('Adult')),
             (JUVENILE, _('Juvenile')),
             (OTHER, _('')),
-        ]
-        sq_age = forms.CharField(
+    ]
+    sq_age = forms.CharField(
             #blank=True,
             max_length=20,
             #choices=AGE_CHOICES,
             #default=OTHER,
-            )
+     )
