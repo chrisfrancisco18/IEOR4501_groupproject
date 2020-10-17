3cb91f36d121e539a2ae8a23e61fd96ca473088e
chrisfrancisco18 <72322765+chrisfrancisco18@users.noreply.github.com>
2020-10-17 19:42:53 -0400

    Update urls.py

--- adopt/urls.py
+++ adopt/urls.py
@@ -7,9 +7,9 @@ app_name = 'adopt'
 extra_patterns = [
         path('', views.sightings, name='sightings'),
         # path('<int:squirrel_id>/', views.detail, name='detail'),
-        path('<slug:Unique_Squirrel_ID>/', views.squirrel_detail, name='detail'),
         # path for stats
         path('stats/', views.stat_acts),
+        path('<slug:Unique_Squirrel_ID>/', views.squirrel_detail, name='detail'),
 ]
 
 urlpatterns = [
