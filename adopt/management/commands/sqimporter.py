33c6b89e5b0614a8e13251564f7144b20feb21eb
Peeratach Ritthikarn <pr2638@columbia.edu>
2020-10-19 20:13:06 +0000

    mess up

---
+++ adopt/management/commands/sqimporter.py
@@ -0,0 +1,88 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from adopt.models import SquirrelTest
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_path', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        filename = options['csv_path'][0]
+
+        with open(filename) as f:
+            reader = csv.DictReader(f)
+
+            for item in reader:
+                x = item['X']
+                y = item['Y']
+                squirrel_id = item['Unique Squirrel ID']
+                hectare = item['Hectare']
+                shift = item['Shift']
+                date = item['Date']
+                hectare_sq = int(item['Hectare Squirrel Number'])
+                age = item['Age']
+                primary_fur = item['Primary Fur Color']
+                highlight_fur = item['Highlight Fur Color']
+                combi_fur = item['Combination of Primary and Highlight Color']
+                color_note = item['Color notes']
+                location = item['Location']
+                above_ground = item['Above Ground Sighter Measurement']
+                spec_loc = item['Specific Location']
+                running = True
+                if item['Running']=='FALSE':
+                    running = False
+                chasing = True
+                if item['Chasing']=='FALSE':
+                    chasing = False
+                climbing = True
+                if item['Climbing']=='FALSE':
+                    climbing = False
+                eating = True
+                if item['Eating']=='FALSE':
+                    eating = False
+                foraging = True
+                if item['Foraging']=='FALSE':
+                    foraging = False
+
+                other_act = item['Other Activities']
+
+                kuks = True
+                if item['Kuks']=='FALSE':
+                    kuks = False
+                quaas = True
+                if item['Quaas']=='FALSE':
+                    quaas = False
+                moans = True
+                if item['Moans']=='FALSE':
+                    moans = False
+                flags = True
+                if item['Tail flags']=='FALSE':
+                    flags = False
+                twitches = True
+                if item['Tail twitches']=='FALSE':
+                    twitches = False
+                approaches = True
+                if item['Approaches']=='FALSE':
+                    approaches = False
+                indifferent = True
+                if item['Indifferent']=='FALSE':
+                    indifferent = False
+                runsfrom = True
+                if item['Runs from']=='FALSE':
+                    runsfrom = False
+
+
+                other_int = item['Other Interactions'] 
+                lat_long = item['Lat/Long']
+
+
+                squirrelTest = SquirrelTest.objects.create_squirrelTest(x, y, squirrel_id, hectare, shift, date, hectare_sq, age, primary_fur, highlight_fur,
+                        combi_fur, color_note, location, above_ground, spec_loc, running, chasing, climbing, eating, foraging, other_act, kuks, quaas, moans, flags, twitches, approaches,
+                       indifferent, runsfrom, other_int, lat_long)
+                squirrelTest.save()
+
+                self.stdout.write(self.style.SUCCESS('Successfully created squirrel "%s"' % squirrel_id))
--- adopt/models.py
+++ adopt/models.py
@@ -2,160 +2,148 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 # Create your models here
+class SquirrelTestManager(models.Manager):
+    def create_squirrelTest(self, x, y, squirrel_id, hectare, shift, date, hectare_sq, age, primary_fur, highlight_fur,combi_fur, color_note, location, above_ground, spec_loc, running,
+            chasing, climbing, eating, foraging, other_act, kuks, quaas, moans, flags, twitches, approaches, indifferent, runsfrom, other_int, lat_long):
 
-class Squirrel(models.Model):
-    Longitude=models.FloatField(help_text=_("Longitude"))
-    Latitude=models.FloatField(help_text=_("Latitude"))
-    
-    """
-    Unique_Squirrel_ID=models.CharField(
-        max_length=14,
-        help_text=_("The Unique ID"),
-    ) 
-    """
-    
-    # use Unique Squirrel ID as a primary key
-    Unique_Squirrel_ID=models.SlugField(
-        primary_key=True,
-        max_length=14,
-        help_text=_("The Unique ID"),
-    )
-    
-    Hectare=models.CharField(
-        blank=True,
-        max_length=4,
-        help_text=_("Hectare"),
-    )
-    
-    AM = 'AM'
+        squirrelTest = self.create(x=x, y=y, squirrel_id=squirrel_id, hectare=hectare, shift=shift,date=date, hectare_sq=hectare_sq,
+                age = age, primary_fur = primary_fur, highlight_fur = highlight_fur, combi_fur = combi_fur, color_note = color_note, location = location, above_ground= above_ground, spec_loc = spec_loc, running = running, chasing= chasing, climbing = climbing, eating= eating, foraging= foraging, other_act=other_act,kuks = kuks, quaas = quaas, moans=moans, flags=flags, twitches = twitches, approaches = approaches, indifferent = indifferent, runsfrom = runsfrom, other_int = other_int, lat_long = lat_long
+                )
+        return squirrelTest
+
+class SquirrelTest(models.Model):
+    x = models.FloatField(
+            default = 0,
+            )
+    y = models.FloatField(
+            default = 0,
+            )
+    squirrel_id = models.SlugField(
+            primary_key=True,
+            max_length=14,
+            )
+    hectare = models.CharField(max_length=4,
+            default = '',
+            )
+    AM='AM'
     PM = 'PM'
-    
-    SHIFT_CHOICES = [
-        (AM, _('AM')),
-        (PM, _('PM')),
-    ]
-    
-    Shift=models.CharField(
-        max_length=3,
-        choices=SHIFT_CHOICES,
-        default=AM,
-    )
-    
-    """
-    Date=models.DateField(
-        help_text=_("Date"),
-    )
-    """
-    Date=models.CharField(
-        max_length=9,
-        help_text=_("Date"),
-    )
-    
-    Hectare_squirrel_nunber=models.IntegerField(
-        blank=True,
-        help_text=_("Hectare Number"),
-    )
-    
+    shift_choices = [
+            (AM, _('AM')),
+            (PM, _('PM')),
+                ]
+    shift = models.CharField(
+            max_length = 3,
+            choices = shift_choices,
+            default = AM,
+            )
+    date = models.CharField(
+            max_length = 9,
+            default = '',
+            )
+    hectare_sq = models.IntegerField(
+            default =0,
+            )
     ADULT = 'Adult'
     JUVENILE = 'Juvenile'
     OTHER = ''
-    
     AGE_CHOICES = [
         (ADULT, _('Adult')),
         (JUVENILE, _('Juvenile')),
         (OTHER, _('')),
     ]
-    
-    Age=models.CharField(
+    age = models.CharField(
         blank=True,
         max_length=20,
-        help_text=_("Age"),
+       # help_text=_("Age"),
         choices=AGE_CHOICES,
-        default=OTHER,
+        default='',
     )
-    
-    Primary_Fur_Color=models.CharField(
-        blank=True,
-        max_length=20,
-        help_text=_("Primary Fur Color"),
-    )
-    
-    Highlight_Fur_Color=models.CharField(
+    primary_fur = models.CharField(
+            max_length = 20,
+            help_text=_("Primary Fur Color"),
+            default='',
+             )
+    highlight_fur =models.CharField(
         blank=True,
         max_length=20,
         help_text=_("Highlight Fur Color"),
+        default='',
     )
-    
-    Combi_Fur_Color=models.CharField(
+    combi_fur = models.CharField(
         blank=True,
         max_length=40,
         help_text=_("Combine Fur Color"),
+        default='',
     )
-    
-    Color_Notes=models.CharField(
+    color_note = models.CharField(
         blank=True,
         max_length=40,
         help_text=_("Color Note"),
+        default='',
     )
-    
+
     AG = 'Above Ground'
     GP = 'Ground Plane'
     OTHER = ''
-    
     LOCATION_CHOICES = [
-        (AG, _('Above Ground')),
+       (AG, _('Above Ground')),
         (GP, _('Ground Plane')),
         (OTHER, _('')),
     ]
-    
-    Location=models.CharField(
+    location =models.CharField(
         max_length=20,
         help_text=_("Locations"),
         choices=LOCATION_CHOICES,
-        default=OTHER,
+        default='',
     )
-    
-    Above_Ground=models.CharField(
+    above_ground =models.CharField(
         blank=True,
         max_length=20,
+        default='',
         help_text=_("How Far from Ground Please Put The Number. If Found On The Ground Please Put FALSE"),
     )
-    
-    Specific_Location=models.TextField(
+    spec_loc = models.CharField(
         blank=True,
+        max_length=70,
+        default='',
         help_text=_("Specific Location"),
     )
-    
-    Running=models.BooleanField()
-    Chasing=models.BooleanField()
-    Climbing=models.BooleanField()
-    Eating=models.BooleanField()
-    Foraging=models.BooleanField()
-    
-    Other_Activ=models.TextField(
+
+    running = models.BooleanField(default=True)
+    chasing = models.BooleanField(default=True)
+    climbing = models.BooleanField(default=True)
+    eating = models.BooleanField(default=True)
+    foraging = models.BooleanField(default=True)
+
+    other_act = models.CharField(
         blank=True,
-        help_text=_("Other Activites"),
+        max_length=30,
+        default='',
+        help_text=_("Report any other activities and leave it empty if there is none"),
     )
-    
-    Kuks=models.BooleanField()
-    Quaas=models.BooleanField()
-    Moans=models.BooleanField()
-    Tail_flags=models.BooleanField()
-    Tail_twitch=models.BooleanField()
-    Approaches=models.BooleanField()
-    Indifferent=models.BooleanField()
-    Runs_from=models.BooleanField()
-    
-    Other_Inter=models.CharField(
+
+    kuks = models.BooleanField(default=True)
+    quaas = models.BooleanField(default=True)
+    moans = models.BooleanField(default=True)
+    flags = models.BooleanField(default=True)
+    twitches = models.BooleanField(default=True)
+    approaches = models.BooleanField(default=True)
+    indifferent = models.BooleanField(default=True)
+    runsfrom = models.BooleanField(default=True)
+
+    other_int = models.CharField(
         blank=True,
-        max_length=20,
-        help_text=_("Other Interactions"),
+        max_length=30,
+        default='',
+        help_text=_("Report any other interaction and leave it empty if there is none"),
     )
-    
-    Lat_Long=models.CharField(
+
+    lat_long = models.CharField(
         blank=True,
-        max_length=100,
-    )
-    
-    def __str__(self):
-        return self.Unique_Squirrel_ID
+        max_length=30,
+        default='',
+        )
+
+
+
+    objects = SquirrelTestManager()
